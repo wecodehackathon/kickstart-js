3e2674bcf90692e5d3b51bf196e6f88c06350b12
Robyn Navarro <rnavarro182@gmail.com>
2019-01-12 11:15:08 -0800

    SubmitForm component

---
+++ client/src/SubmitForm.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class SubmitForm extends Component {
+
+  state = {
+    title: '',
+    textBody: '',
+    tags: ''
+  }
+  render() {
+    return (
+      <div>
+        <h2>Create a post</h2>
+        <form onSubmit={this.handleSubmit}>
+        <label>
+          <h4>Title</h4>
+          <input type="text"/>
+        </label>
+        <label>
+          <h4>Body</h4>
+          <textarea></textarea>
+        </label>
+        <label>
+          <h4>Tags</h4>
+          {/* Tags component here */}
+        </label>
+        <button>Submit</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default SubmitForm;
