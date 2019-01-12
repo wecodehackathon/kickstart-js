8ed702690560bc08ed280ccb88f406a1b8940a5f
Claire Flanagan <claireflanagan@gmail.com>
2019-01-12 14:14:40 -0800

    form styling and complete box styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
-import ListItem from './ListItem.js';
 import Header from './Header';
 import Complete from './Complete';
+import Footer from './Footer';
 
 import TextNotifierForm from './TextNotifierForm';
 
 class App extends Component {
   state = {
-    showThankyou: false,
+    showThankYou: true
   };
+
   setThankYou = () => {
     this.setState({ showThankYou: true })
   }
+
   render() {
    return (
     <div>
       <Header />
       {!this.state.showThankYou && <TextNotifierForm setThankYou={this.setThankYou} />}
-      {this.state.showThankyou && <div>
-        thansk you
-        </div>}
-     </div>)
+      {this.state.showThankYou && <Complete />}
+      <Footer />
+    </div>)
   }
 }
 
--- client/src/Complete.js
+++ client/src/Complete.js
@@ -3,7 +3,7 @@ import './App.css';
 
 export default function Complete() {
   return (
-    <div class="complete">
+    <div className="complete">
       <h2>Thank you for signing up to receive SUN school class reminders!</h2>
       <h3>You'll receive a welcome message on your phone shortly and notifications about SUN classes your child is enrolled in the evening before they are scheduled.</h3>
     </div>
