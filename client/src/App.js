33a937bb9be668daca0cbc29b6ed9b3f20773f5f
Sarah Steinberg <sarah888@gmail.com>
2019-01-12 13:13:46 -0800

    ...

--- client/src/App.js
+++ client/src/App.js
@@ -4,51 +4,18 @@ import TextNotifierForm from './TextNotifierForm';
 
 class App extends Component {
   state = {
-    list: '',
-    post: '',
-    responseToPost: ''
+    showThankyou: false,
   };
-/*
-  componentDidMount() {
-    this.updateTodoList();
+  setThankYou = () => {
+    this.setState({ showThankYou: true })
   }
-
-  updateTodoList() {
-    this.callApi()
-      .then(res => this.setState({ list: res.toDoList }))
-      .catch(err => console.log(err));
-  }
-
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
-
-    if (response.status !== 200) throw Error(body.message);
-
-    return body;
-  };
-
-  handleSubmit = async e => {
-    e.preventDefault();
-    const response = await fetch('/api/addItem', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ post: this.state.post })
-    });
-    const body = await response.text()
-     .then(this.updateTodoList());
-
-    this.setState({ responseToPost: body, post: '' });
-  };
-*/
   render() {
-    console.log("alksdfjalskdfjalskdfjalsdkfj....")
    return (
     <div>
-      <TextNotifierForm />
-      {/* <div>hello world....</div> */}
+      {!this.state.showThankYou && <TextNotifierForm setThankYou={this.setThankYou} />}
+      {this.state.showThankyou && <div>
+        thansk you
+        </div>}
      </div>)
   }
 }
